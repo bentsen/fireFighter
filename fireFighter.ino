a05a4387eddee9639a1300a58ab7340c8ea55b19
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2022-11-01 17:26:23 +0100

    yepper

--- fireFighter.ino
+++ fireFighter.ino
@@ -4,21 +4,58 @@ const int pwmMotorB = D2;
 const int dirMotorA = D3;
 const int dirMotorB = D4;
 const int waterPump = D5;
-
+bool fire = false;
+int Flame = HIGH;
+int motorSpeed = 500;
 
 void setup() {
   pinMode(waterPump, OUTPUT);
-  pinMode(pwnMotorA, OUTPUT);
+  pinMode(pwmMotorA, OUTPUT);
   pinMode(pwmMotorB, OUTPUT);
   pinMode(dirMotorA, OUTPUT);
   pinMode(dirMotorB, OUTPUT);
   pinMode(flameSensor, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void handle_fire() {
-  digitialWrite()
+  delay(500);
+  digitalWrite(pwmMotorA, 0);
+  digitalWrite(pwmMotorB, 0);
+  digitalWrite(dirMotorA, HIGH);
+  digitalWrite(dirMotorB, HIGH);
+
+  //kør pumpe
+  delay(500);
+  digitalWrite (waterPump, LOW);
+  delay(2000);
+  digitalWrite(waterPump, HIGH);
+
+  fire == false;
 }
 
 void loop() {
-  digitalWrite(waterPump, LOW);
+  Flame = digitalRead(flameSensor);
+
+  if(Flame == LOW) {
+    digitalWrite(pwmMotorA, motorSpeed);
+    digitalWrite(pwmMotorB, motorSpeed);
+    digitalWrite(dirMotorA, LOW);
+    digitalWrite(dirMotorB, LOW);  
+    fire == true;
+  }
+  else{
+    digitalWrite(pwmMotorA, 0);
+    digitalWrite(pwmMotorB, 0);
+    digitalWrite(dirMotorA, HIGH);
+    digitalWrite(dirMotorB, HIGH);
+    fire == false;
+  }
+
+  delay(300);
+  while(fire == true){
+     handle_fire();
+  }
 }
